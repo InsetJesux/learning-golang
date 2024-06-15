81058c37336d9da67363490c0e036618b4a1225c
InsetJesux <insetjesux@gmail.com>
2024-06-15 16:53:03 +0200

    docs: add ExampleAdd testeable example

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,4 +12,13 @@ func TestAdder(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+// Ejemplo testeable, permite añadir ejemplos a la documentación generada por godoc.
+// Estos ejemplos se compilan por lo que te asegura que al actualizar el código fuente los ejemplos siguen funcionando
+// Añadir un comentario con 'Output' en el ejemplo hará que se ejecute como un test
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
